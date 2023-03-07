6659097c004c57b26ab12e50c816b30574d01b5e
Jung-Bin Park <83396015+Jungbin-Park@users.noreply.github.com>
2023-03-07 16:32:54 +0900

    공격 범위 설정
    범위 안에 들어오면 기즈모 색상 변경

--- Assets/02.scripts/TowerCtrl.cs
+++ Assets/02.scripts/TowerCtrl.cs
@@ -20,6 +20,7 @@ public class TowerCtrl : MonoBehaviour
 
     public float attackDist = 30f;
     public float lineSize = 30f;
+    public bool isGame = true;
     public Object particle;
 
     RaycastHit hit;
@@ -30,62 +31,60 @@ public class TowerCtrl : MonoBehaviour
         GameObject[] arrEnemy = GameObject.FindGameObjectsWithTag("MONSTER");
         towerTr = GetComponent<Transform>();
         anim.GetComponent<Animator>();
-    }
-
-    void Update()
-    {
         StartCoroutine(UpdateTarget());
     }
 
     // 가장 가까운 적 감지
     IEnumerator UpdateTarget()
     {
-        GameObject[] arrEnemy = GameObject.FindGameObjectsWithTag("MONSTER");
-        float shortestDistance = Mathf.Infinity;
-        GameObject nearestEnemy = null;
-        foreach (GameObject enemy in arrEnemy)
+        while (isGame)
         {
-            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
-            if (distanceToEnemy < shortestDistance)
+            // 감지된 몬스터 배열에 저장
+            GameObject[] arrEnemy = GameObject.FindGameObjectsWithTag("MONSTER");
+            // 최소거리 = 무한대
+            float shortestDistance = Mathf.Infinity;
+            // 가까운 적 게임오브젝트
+            GameObject nearestEnemy = null;
+            foreach (GameObject enemy in arrEnemy)
             {
-                shortestDistance = distanceToEnemy;
-                nearestEnemy = enemy;
-            }
-        }
-
-        if (nearestEnemy != null && shortestDistance <= attackDist)
-        {
-            target = nearestEnemy.transform;
-        }
-        else
-        {
-            target = null;
-        }
-
-        if(nearestEnemy != null)
-        {
-            towerTr.LookAt(target);
-            Debug.DrawRay(transform.position, transform.forward * lineSize, Color.green);
-
-            // 가장 광선과 가까운 1개의 오브젝트만 감지한다.
-            // 물체에 닿으면 true를 반환
-            if (Physics.Raycast(transform.position, transform.forward, out hit, lineSize))
-            {
-                if (hit.collider.tag.Equals("MONSTER"))
+                // 적과 포탑의 거리
+                float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+                if (distanceToEnemy < shortestDistance)
                 {
-                    doShot();
+                    shortestDistance = distanceToEnemy;
+                    nearestEnemy = enemy;
                 }
             }
+            // 공격 사정거리 안에 있으면 타겟 설정
+            if (nearestEnemy != null && shortestDistance <= attackDist)
+            {
+                target = nearestEnemy.transform;
+            }
+            else
+            {
+                target = null;
+            }
+
+            if (target != null)
+            {
+                state = State.ATTACK;
+                towerTr.LookAt(target);
+                Debug.DrawRay(transform.position, transform.forward * lineSize, Color.green);
+                // 가장 광선과 가까운 1개의 오브젝트만 감지한다. 물체에 닿으면 true를 반환
+                if (Physics.Raycast(transform.position, transform.forward, out hit, lineSize))
+                {
+                    if (hit.collider.tag.Equals("MONSTER"))
+                    {
+                        StartCoroutine(coFire());
+                    }
+                }
+            }
+            yield return null;
         }
-        yield return null;
+        
     }
 
-    private void doShot()
-    {
-        StartCoroutine(coShot());
-    }
-
-    IEnumerator coShot()
+    IEnumerator coFire()
     {
         yield return new WaitForSeconds(1.0f);
         // 닿은 물체의 이름을 출력
@@ -95,7 +94,16 @@ public class TowerCtrl : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, attackDist);
+        if(state == State.IDLE)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, attackDist);
+        }
+        if(state == State.ATTACK)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, attackDist);
+        }
+        
     }
 }
