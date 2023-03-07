854c5f7a257ebb2b04b63b2913c7eed26abd0252
Jung-Bin Park <83396015+Jungbin-Park@users.noreply.github.com>
2023-03-07 17:08:29 +0900

    타워 공격 애니메이션 추가

--- Assets/02.scripts/TowerCtrl.cs
+++ Assets/02.scripts/TowerCtrl.cs
@@ -14,7 +14,6 @@ public class TowerCtrl : MonoBehaviour
     public State state = State.IDLE;
 
     private Animator anim;
-    private Transform enemyTr;
     private Transform towerTr;
     private Transform target;
 
@@ -24,64 +23,70 @@ public class TowerCtrl : MonoBehaviour
     public Object particle;
 
     RaycastHit hit;
-
     void Start()
     {
-        //enemyTr = GameObject.FindGameObjectWithTag("MONSTER").GetComponent<Transform>();
-        GameObject[] arrEnemy = GameObject.FindGameObjectsWithTag("MONSTER");
         towerTr = GetComponent<Transform>();
-        anim.GetComponent<Animator>();
-        StartCoroutine(UpdateTarget());
+        anim = GetComponent<Animator>();
     }
-
+    private void Update()
+    {
+        StartCoroutine(UpdateTarget());
+        StartCoroutine(stateAction());
+    }
     // 가장 가까운 적 감지
     IEnumerator UpdateTarget()
     {
-        while (isGame)
+        // 감지된 몬스터 배열에 저장
+        GameObject[] arrEnemy = GameObject.FindGameObjectsWithTag("MONSTER");
+        // 최소거리 = 무한대
+        float shortestDistance = Mathf.Infinity;
+        // 가까운 적 게임오브젝트
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in arrEnemy)
         {
-            // 감지된 몬스터 배열에 저장
-            GameObject[] arrEnemy = GameObject.FindGameObjectsWithTag("MONSTER");
-            // 최소거리 = 무한대
-            float shortestDistance = Mathf.Infinity;
-            // 가까운 적 게임오브젝트
-            GameObject nearestEnemy = null;
-            foreach (GameObject enemy in arrEnemy)
+            // 적과 포탑의 거리
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
             {
-                // 적과 포탑의 거리
-                float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
-                if (distanceToEnemy < shortestDistance)
-                {
-                    shortestDistance = distanceToEnemy;
-                    nearestEnemy = enemy;
-                }
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
             }
-            // 공격 사정거리 안에 있으면 타겟 설정
-            if (nearestEnemy != null && shortestDistance <= attackDist)
-            {
-                target = nearestEnemy.transform;
-            }
-            else
-            {
-                target = null;
-            }
-
-            if (target != null)
-            {
-                state = State.ATTACK;
-                towerTr.LookAt(target);
-                Debug.DrawRay(transform.position, transform.forward * lineSize, Color.green);
-                // 가장 광선과 가까운 1개의 오브젝트만 감지한다. 물체에 닿으면 true를 반환
-                if (Physics.Raycast(transform.position, transform.forward, out hit, lineSize))
-                {
-                    if (hit.collider.tag.Equals("MONSTER"))
-                    {
-                        StartCoroutine(coFire());
-                    }
-                }
-            }
-            yield return null;
         }
-        
+        // 공격 사정거리 안에 있으면 타겟 설정
+        if (nearestEnemy != null && shortestDistance <= attackDist)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+
+        if (target != null)
+        {
+            state = State.ATTACK;
+            //anim.SetBool("isAttack", true);
+            towerTr.LookAt(target);
+            Debug.DrawRay(transform.position, transform.forward * lineSize, Color.green);
+            // 가장 광선과 가까운 1개의 오브젝트만 감지한다. 물체에 닿으면 true를 반환
+            if (Physics.Raycast(transform.position, transform.forward, out hit, lineSize))
+            {
+                if (hit.collider.tag.Equals("MONSTER"))
+                {
+                    StartCoroutine(coFire());
+                }
+            }
+        }
+        else
+        {
+            state = State.IDLE;
+        }
+
+        if (arrEnemy == null)
+        {
+            isGame = false;
+        }
+        yield return null;
     }
 
     IEnumerator coFire()
@@ -90,6 +95,21 @@ public class TowerCtrl : MonoBehaviour
         // 닿은 물체의 이름을 출력
         //Debug.Log(hit.collider.gameObject.name);
         hit.transform.GetComponent<EnemyCtrl>()?.OnDamage(hit.point, hit.normal);
+    }
+
+    IEnumerator stateAction()
+    {
+        switch (state)
+        {
+            case State.IDLE:
+                anim.SetBool("isAttack", false);
+                break;
+            case State.ATTACK:
+                Debug.Log("isAttack True!");
+                anim.SetBool("isAttack", true);
+                break;
+        }
+        yield return null;
     }
 
     private void OnDrawGizmos()
